a5730de1dcf0e0a5e3eb60443587f763ea66d8a1
JohnChourp <jonhiedarn@gmail.com>
2024-10-19 17:58:56 +0300

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.learnbyzantinemusic',
+  appName: 'learn-byzantine-music',
+  webDir: 'www'
+};
+
+export default config;
